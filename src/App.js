ce60367f14f1f7859fbd020d11cd3edf561726c2
Josh Darling <squiddarling@gmail.com>
2023-03-09 02:07:10 -0500

    removed boiler plate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  // const [nilist,setnilist] = useState([]);
+
+return(
+<div>
+<h1>Foo</h1>
+We are the knight who say...
+<h1>NI!</h1>
+</div>
+);
+
 }
 
 export default App;
